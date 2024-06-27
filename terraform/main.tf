1a1c9582c4267d6421a9d7f70db9cbb07e5ad46c
janehojy <janehojy@gmail.com>
2024-06-27 11:03:47 +0800

    add actions

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=3.42.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_s3_bucket" "jane" {
+  bucket = "jane-bucket-1234"
+
+  tags = {
+    Name        = "jane-bucket"
+  }
+}
